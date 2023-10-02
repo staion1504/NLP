3c0958ce4e0e7eba6c8775ad2a18d7cd2f325a07
staion1504 <pagillasaitejauma@gmail.com>
2023-10-02 14:25:47 +0530

    First time git

---
+++ main.py
@@ -0,0 +1,364 @@
+import re
+import csv
+def lemmatize(word):
+    lemmatized_word = word.lower()
+    irregular_words = {
+        "running": "run",
+        "flies": "fly",
+        "happily": "happy",
+        "generously": "generous",
+        "better": "good",
+        "mice": "mouse",
+        "oxen": "ox",
+        "geese": "goose",
+        "teeth": "tooth",
+        "feet": "foot",
+        "is": "be",
+        "was": "be",
+        "am": "be",
+        "has": "have",
+        "had": "have",
+        "does": "do",
+        "did": "do",
+        "goes": "go",
+        "went": "go",
+        "goose": "geese",
+        "child": "children",
+        "man": "men",
+        "woman": "women",
+        "person": "people",
+        "leaf": "leaves",
+        "amazing": "amaze",
+        "delicious": "delight",
+        "excellent": "excel",
+        "fantastic": "fantasy",
+        "horrible": "horror",
+        "terrible": "terror",
+        "wonderful": "wonder",
+        "outstanding": "outstand",
+        "impressive": "impress",
+        "disappointing": "disappoint",
+        "satisfying": "satisfy",
+        "reviews": "review",
+        "feedbacks": "feedback",
+        "customers": "customer",
+        "products": "product",
+        "experience": "experience",
+        "service": "service",
+        "quality": "quality",
+        "positive": "posit",
+        "negative": "negat",
+        "suggestion": "suggest",
+        "improvement": "improve",
+        "learning":"learn"
+    }
+
+    if lemmatized_word in irregular_words:
+        return irregular_words[lemmatized_word]
+
+    return lemmatized_word
+
+
+def transform_contractions(text):
+    contractions_expanded = { 
+"ain't": "am not",
+"aren't": "are not",
+"can't": "cannot",
+"can't've": "cannot have",
+"'cause": "because",
+"could've": "could have",
+"couldn't": "could not",
+"couldn't've": "could not have",
+"didn't": "did not",
+"doesn't": "does not",
+"doesn’t": "does not",
+"don't": "do not",
+"don’t": "do not",
+"hadn't": "had not",
+"hadn't've": "had not have",
+"hasn't": "has not",
+"haven't": "have not",
+"he'd": "he had",
+"he'd've": "he would have",
+"he'll": "he will",
+"he'll've": "he will have",
+"he's": "he is",
+"how'd": "how did",
+"how'd'y": "how do you",
+"how'll": "how will",
+"how's": "how is",
+"i'd": "i would",
+"i'd've": "i would have",
+"i'll": "i will",
+"i'll've": "i will have",
+"i'm": "i am",
+"i've": "i have",
+"isn't": "is not",
+"it'd": "it would",
+"it'd've": "it would have",
+"it'll": "it will",
+"it'll've": "it will have",
+"it's": "it is",
+"let's": "let us",
+"ma'am": "madam",
+"mayn't": "may not",
+"might've": "might have",
+"mightn't": "might not",
+"mightn't've": "might not have",
+"must've": "must have",
+"mustn't": "must not",
+"mustn't've": "must not have",
+"needn't": "need not",
+"needn't've": "need not have",
+"o'clock": "of the clock",
+"oughtn't": "ought not",
+"oughtn't've": "ought not have",
+"shan't": "shall not",
+"sha'n't": "shall not",
+"shan't've": "shall not have",
+"she'd": "she would",
+"she'd've": "she would have",
+"she'll": "she will",
+"she'll've": "she will have",
+"she's": "she is",
+"should've": "should have",
+"shouldn't": "should not",
+"shouldn't've": "should not have",
+"so've": "so have",
+"so's": "so is",
+"that'd": "that would",
+"that'd've": "that would have",
+"that's": "that is",
+"there'd": "there would",
+"there'd've": "there would have",
+"there's": "there is",
+"they'd": "they would",
+"they'd've": "they would have",
+"they'll": "they will",
+"they'll've": "they will have",
+"they're": "they are",
+"they've": "they have",
+"to've": "to have",
+"wasn't": "was not",
+"we'd": "we would",
+"we'd've": "we would have",
+"we'll": "we will",
+"we'll've": "we will have",
+"we're": "we are",
+"we've": "we have",
+"weren't": "were not",
+"what'll": "what will",
+"what'll've": "what will have",
+"what're": "what are",
+"what's": "what is",
+"what've": "what have",
+"when's": "when is",
+"when've": "when have",
+"where'd": "where did",
+"where's": "where is",
+"where've": "where have",
+"who'll": "who will",
+"who'll've": "who will have",
+"who's": "who is",
+"who've": "who have",
+"why's": "why is",
+"why've": "why have",
+"will've": "will have",
+"won't": "will not",
+"won't've": "will not have",
+"would've": "would have",
+"wouldn't": "would not",
+"wouldn't've": "would not have",
+"y'all": "you all",
+"y’all": "you all",
+"y'all'd": "you all would",
+"y'all'd've": "you all would have",
+"y'all're": "you all are",
+"y'all've": "you all have",
+"you'd": "you would",
+"you'd've": "you would have",
+"you'll": "you will",
+"you'll've": "you will have",
+"you're": "you are",
+"you've": "you have",
+"ain’t": "am not",
+"aren’t": "are not",
+"can’t": "cannot",
+"can’t’ve": "cannot have",
+"’cause": "because",
+"could’ve": "could have",
+"couldn’t": "could not",
+"couldn’t’ve": "could not have",
+"didn’t": "did not",
+"doesn’t": "does not",
+"don’t": "do not",
+"don’t": "do not",
+"hadn’t": "had not",
+"hadn’t’ve": "had not have",
+"hasn’t": "has not",
+"haven’t": "have not",
+"he’d": "he had",
+"he’d’ve": "he would have",
+"he’ll": "he will",
+"he’ll’ve": "he will have",
+"he’s": "he is",
+"how’d": "how did",
+"how’d’y": "how do you",
+"how’ll": "how will",
+"how’s": "how is",
+"i’d": "i would",
+"i’d’ve": "i would have",
+"i’ll": "i will",
+"i’ll’ve": "i will have",
+"i’m": "i am",
+"i’ve": "i have",
+"isn’t": "is not",
+"it’d": "it would",
+"it’d’ve": "it would have",
+"it’ll": "it will",
+"it’ll’ve": "it will have",
+"it’s": "it is",
+"let’s": "let us",
+"ma’am": "madam",
+"mayn’t": "may not",
+"might’ve": "might have",
+"mightn’t": "might not",
+"mightn’t’ve": "might not have",
+"must’ve": "must have",
+"mustn’t": "must not",
+"mustn’t’ve": "must not have",
+"needn’t": "need not",
+"needn’t’ve": "need not have",
+"o’clock": "of the clock",
+"oughtn’t": "ought not",
+"oughtn’t’ve": "ought not have",
+"shan’t": "shall not",
+"sha’n’t": "shall not",
+"shan’t’ve": "shall not have",
+"she’d": "she would",
+"she’d’ve": "she would have",
+"she’ll": "she will",
+"she’ll’ve": "she will have",
+"she’s": "she is",
+"should’ve": "should have",
+"shouldn’t": "should not",
+"shouldn’t’ve": "should not have",
+"so’ve": "so have",
+"so’s": "so is",
+"that’d": "that would",
+"that’d’ve": "that would have",
+"that’s": "that is",
+"there’d": "there would",
+"there’d’ve": "there would have",
+"there’s": "there is",
+"they’d": "they would",
+"they’d’ve": "they would have",
+"they’ll": "they will",
+"they’ll’ve": "they will have",
+"they’re": "they are",
+"they’ve": "they have",
+"to’ve": "to have",
+"wasn’t": "was not",
+"we’d": "we would",
+"we’d’ve": "we would have",
+"we’ll": "we will",
+"we’ll’ve": "we will have",
+"we’re": "we are",
+"we’ve": "we have",
+"weren’t": "were not",
+"what’ll": "what will",
+"what’ll’ve": "what will have",
+"what’re": "what are",
+"what’s": "what is",
+"what’ve": "what have",
+"when’s": "when is",
+"when’ve": "when have",
+"where’d": "where did",
+"where’s": "where is",
+"where’ve": "where have",
+"who’ll": "who will",
+"who’ll’ve": "who will have",
+"who’s": "who is",
+"who’ve": "who have",
+"why’s": "why is",
+"why’ve": "why have",
+"will’ve": "will have",
+"won’t": "will not",
+"won’t’ve": "will not have",
+"would’ve": "would have",
+"wouldn’t": "would not",
+"wouldn’t’ve": "would not have",
+"y’all": "you all",
+"y’all": "you all",
+"y’all’d": "you all would",
+"y’all’d’ve": "you all would have",
+"y’all’re": "you all are",
+"y’all’ve": "you all have",
+"you’d": "you would",
+"you’d’ve": "you would have",
+"you’ll": "you will",
+"you’ll’ve": "you will have",
+"you’re": "you are",
+"you’re": "you are",
+"you’ve": "you have",
+}
+
+    words = text.split()
+    transformed_words = []
+    for word in words:
+        if word in contractions_expanded:
+            transformed_words.extend(contractions_expanded[word].split())
+        else:
+            transformed_words.append(word)
+    return ' '.join(transformed_words)
+
+
+
+
+
+
+def preprocess_text(text, remove_stopwords=True):
+    text = text.lower()
+    text = transform_contractions(text)
+    text = re.sub(r'https?:\/\/.[\r\n]', '', text, flags=re.MULTILINE)
+    text = re.sub(r'\<a href', ' ', text)
+    text = re.sub(r'[_"\-;%()|+&=*%.,!?:#$@\[\]/]', ' ', text) #special chars and punct
+    text = re.sub(r'<br />', ' ', text)
+    text = re.sub(r'\'', ' ', text)
+    text = re.sub(r'>', ' ', text)
+    return stopword(text)
+
+
+
+
+stopwords = set(["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"])
+
+def stopword(text):
+     str=""
+     word=text.split()
+     for eachWord in word:
+          if eachWord not in stopwords:
+               str=str+eachWord+" "
+
+     return str
+
+
+with open('./Reviews.csv', 'r', newline='') as reviewsFile:
+    reviewsLines = csv.reader(reviewsFile)
+    
+    for index, line in enumerate(reviewsLines):
+        if index == 0:
+            continue
+        elif index == 5:
+            break
+        else:
+            # print(line)
+            linex = line[9]
+            print("--------------------------------------------------")
+            print("Original text:", linex)
+            print("-------------------------------------------------")
+            print("Cleaned text:", preprocess_text(linex))
+            print("-------------------------------------------------")
+               
+
+
